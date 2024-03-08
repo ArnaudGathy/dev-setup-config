a5c660081526061fcfbe19594a8f56cee6fac345
Arnaud Gathy <33348914+ArnaudGathy@users.noreply.github.com>
2024-03-08 09:40:10 +0100

    Update brew-install-formulae.sh

--- brew/brew-install-formulae.sh
+++ brew/brew-install-formulae.sh
@@ -3,12 +3,12 @@ set -e
 
 brew install git
 brew install --cask iterm2
-brew install --cask spectacle
+brew install --cask rectangle
 brew install --cask caffeine
 brew install --cask fork
 brew install --cask spotify
 brew install --cask sublime-text
 brew install --cask visual-studio-code
 brew install --cask webstorm
-brew install --cask ngrok
-brew install --cask slack
+#brew install --cask ngrok
+#brew install --cask slack
